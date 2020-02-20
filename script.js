d6b56912aebbbf15281a182791d08a2c7ec93523
lewisclark4 <lewis-clark@hotmail.co.uk>
2020-02-20 20:39:52 +0000

    added table pagination

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-
-const baseURL = "https://swapi.co/api/";
-
-function getData(type, cb) {
+  
+function getData(url, cb) {
     var xhr = new XMLHttpRequest();
 
     xhr.onreadystatechange = function() {
@@ -10,26 +8,43 @@ function getData(type, cb) {
         }
     };
 
-    xhr.open("GET", baseURL + type + "/");
+    xhr.open("GET", url);
     xhr.send();
 }
 
 
 function getTableHeaders(obj) {
-    var tableHeaders = []
+    var tableHeaders = [];
 
-       Object.keys(obj).forEach(function(key) {;
-       tableHeaders.push(`<td>${key}</td>`) 
-});
+    Object.keys(obj).forEach(function(key) {
+        tableHeaders.push(`<td>${key}</td>`);
+    });
 
     return `<tr>${tableHeaders}</tr>`;
 }
 
-function writeToDocument(type) {
+function generatePaginationButtons(next, prev) {
+    if (next && prev) {
+        return  `<button onclick="writeToDocument('${prev}')">Previous</button>
+                <button onclick="writeToDocument('${next}')">Next</button>`;
+    } else if (next && !prev) {
+        return  `<button onclick="writeToDocument('${next}')">Next</button>`;
+    } else if (!next && prev) {
+        return  `<button onclick="writeToDocument('${prev}')">Previous</button>`;
+    }
+}
+
+
+function writeToDocument(url) {
     var el = document.getElementById("data");
     el.innerHTML = "";
 
-    getData(type, function(data) {
+
+    getData(url, function(data) {
+
+        if (data.next || data.previous) {
+        pagination = generatePaginationButtons(data.next, data.previous)
+    }
         var tableRows = [];
         data = data.results;
         var tableHeaders = getTableHeaders(data[0]);
@@ -46,6 +61,6 @@ function writeToDocument(type) {
         tableRows.push(`<tr>${dataRow}</tr>`);
     });
 
-        el.innerHTML = `<table>${tableHeaders}${tableRows}</table>`
+        el.innerHTML = `<table>${tableHeaders}${tableRows}</table>${pagination}`
     });
 }
